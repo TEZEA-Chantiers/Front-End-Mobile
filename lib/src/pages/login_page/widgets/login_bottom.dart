0088d2491c4d547bbe331e476867fad696309b32
14004306 <MotDePasse!>
2021-02-24 12:58:59 +0100

    Adjusted colors to match global theme

--- lib/src/pages/login_page/widgets/login_bottom.dart
+++ lib/src/pages/login_page/widgets/login_bottom.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:tezea_chantiers/src/pages/chantier_list_page/chantier_list_page.dart';
 import 'package:tezea_chantiers/src/services/crud/authentification.dart';
 import 'package:tezea_chantiers/src/services/crud/utilisateur_service.dart';
+import 'package:tezea_chantiers/src/widgets_generic/color_bank.dart';
 
 import '../models/login_input_controller_model.dart';
 
@@ -19,6 +21,7 @@ class LoginBottom extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            const Padding(padding: EdgeInsets.only(top: 5)),
             RaisedButton.icon(
               onPressed: () {
                 if (context
@@ -36,9 +39,10 @@ class LoginBottom extends StatelessWidget {
                         .passwordController
                         .text
                         .trim(),
-                  ).then((value) {
+                  ).then((token) {
                     FocusScope.of(context).unfocus();
-                    if (value == null) {
+                    print(token);
+                    if (token == null) {
                       String auth_error = "La connexion au compte a échoué.";
                       //Good lord, it seems to be deprecated.
                       Scaffold.of(context).showSnackBar(SnackBar(
@@ -46,21 +50,29 @@ class LoginBottom extends StatelessWidget {
                       ));
                     } else {
                       //print("Retrieved JWT : " + value.jwt);
-                      Utilisateur_Service.getUtilisateur(value.jwt).then(
+                      Utilisateur_Service.getUtilisateur(token.jwt).then(
                           (utilisateur) => AuthentificationService.currentUser =
                               utilisateur);
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) =>
+                                  ChantierListPage()));
                     }
                   });
                 }
               },
-              icon: const Icon(Icons.arrow_forward_rounded),
-              label: const Text('Connexion'),
-              color: Colors.deepOrangeAccent,
+              icon: const Icon(Icons.arrow_forward_rounded, color: ColorBank.CARD_COLOR,),
+              label: Text.rich(TextSpan(
+                text: 'Connexion',
+                style: TextStyle(color: ColorBank.CARD_COLOR),
+              )),
+              color: ColorBank.APP_BAR_COLOR,
             ),
             const Padding(padding: EdgeInsets.only(top: 5)),
-            const Text.rich(TextSpan(
+            Text.rich(TextSpan(
               text: 'En cas d\'oubli, contactez l\'administration. ',
-              style: TextStyle(color: Colors.white70),
+              style: TextStyle(color: Colors.grey[600]),
             )),
           ],
         ),
